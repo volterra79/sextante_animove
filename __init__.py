de09453d449c42ad3049ece0e7dffcbf2c5ea173
francesco <francesco@francesco.(none)>
2012-12-27 19:22:39 +0100

    first version of MCP

---
+++ __init__.py
@@ -0,0 +1,19 @@
+def name():
+    return "AniMove for SEXTANTE"
+def description():
+    return "MCP and Kernel functions for UD"
+def version():
+    return "Version 1.0"
+def icon():
+    return "icons/animalmove.png"
+def qgisMinimumVersion():
+    return "1.0"
+def author():
+    return "Francesco Boccacci"
+def email():
+    return "francescoboccacci@libero.it"
+def classFactory(iface):
+    from animoveProviderPlugin import animoveProviderPlugin
+    return animoveProviderPlugin()
+
+
---
+++ animoveAlgorithmProvider.py
@@ -0,0 +1,34 @@
+import os
+
+from PyQt4 import QtGui
+
+from sextante.core.AlgorithmProvider import AlgorithmProvider
+
+from mcp import mcp
+#from sextante.animove.href import href
+
+
+class animoveAlgorithmProvider(AlgorithmProvider):
+
+    def __init__(self):
+        AlgorithmProvider.__init__(self)
+        self.alglist = [mcp()]
+                        #,href()]
+
+    def getDescription(self):
+        return "AniMove (MCP and Kernel analysis UD)"
+
+    def getName(self):
+        return "AniMove"
+
+    def getIcon(self):
+        return  QtGui.QIcon(os.path.dirname(__file__) + "/icons/animalmove.png")
+
+    def _loadAlgorithms(self):
+        self.algs = self.alglist
+
+    def getSupportedOutputTableExtensions(self):
+        return ["csv"]
+
+    def supportsNonFileBasedOutput(self):
+        return True
